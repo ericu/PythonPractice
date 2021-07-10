c8239d52797352db4e2548d4b256f058845966dc
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-09 21:05:53 -0700

    Add pattern iterator.

--- siteswap.py
+++ siteswap.py
@@ -2,10 +2,14 @@
 import unittest
 import argparse
 import sys
+from collections import namedtuple
+from more_itertools import take
 
 class InputError(Exception):
   def __init__(self, message):
     self.message = message
+
+Throw = namedtuple('Throw', ('index', 'height'))
 
 class SiteSwap:
   """Class for representing site-swap juggling patterns."""
@@ -21,8 +25,8 @@ class SiteSwap:
     if int(balls) != balls:
       raise InputError(f'Pattern {pattern} uses fractional ball count {balls}.')
     destinations = [False] * length
-    for index, throw in enumerate(pattern):
-      destination = (index + throw) % length
+    for index, height in enumerate(pattern):
+      destination = (index + height) % length
       if destinations[destination]:
         raise InputError(f'Pattern {pattern} has a collision.')
       destinations[destination] = True
@@ -38,10 +42,33 @@ class SiteSwap:
   def __init__(self, parsedPattern):
     self.balls = SiteSwap.validate_pattern(parsedPattern)
     self.pattern = parsedPattern
-    self.hands = 2 # for now
 
   def __repr__(self):
     return f'SiteSwap({self.pattern!r})'
+
+  # TODO: Is there a way to do this more cleanly with a generator?
+  class Iterator:
+    """Class for running a pattern forever."""
+
+    def __init__(self, pattern, balls):
+      self.pattern = pattern
+      self.balls = pattern
+      self.next_throw = 0
+
+    def __iter__(self):
+      return self
+
+    def __next__(self):
+      return self.next()
+
+    def next(self):
+      index = self.next_throw
+      height = self.pattern[index]
+      self.next_throw = (self.next_throw + 1) % len(self.pattern)
+      return Throw(index, height)
+
+  def iterator(self):
+    return self.Iterator(self.pattern, self.balls)
 
 class TestValidatePattern(unittest.TestCase):
   def test_simple_patterns(self):
@@ -65,6 +92,18 @@ class TestValidatePattern(unittest.TestCase):
     self.assertEqual(repr(SiteSwap([4, 4, 1])),
                      repr(SiteSwap.from_string("4, 4,1")))
 
+  def test_iterator(self):
+    iterator = SiteSwap([4, 4, 1]).iterator()
+    throws1 = take(4, iterator)
+    self.assertEqual(throws1[0].height, 4)
+    self.assertEqual(throws1[0].index, 0)
+    self.assertEqual(throws1[1].height, 4)
+    self.assertEqual(throws1[1].index, 1)
+    self.assertEqual(throws1[2].height, 1)
+    self.assertEqual(throws1[2].index, 2)
+    self.assertEqual(throws1[3].height, 4)
+    self.assertEqual(throws1[3].index, 0)
+
 def get_args():
   name = sys.argv[0]
   parser = argparse.ArgumentParser()
@@ -79,4 +118,10 @@ if __name__ == '__main__':
   if args.unittest:
     unittest.main()
   elif args.test:
-    print(SiteSwap.from_string('4,4, 1'))
+    iterator = SiteSwap([4, 4, 1]).iterator()
+    counter = 0
+    for throw in iterator:
+      counter += 1
+      print(throw)
+      if counter > 5:
+        sys.exit()
