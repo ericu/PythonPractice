f3372314c5b15c1b85a3f58640420d32364413f4
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-09 18:54:19 -0700

    Cleanup.

--- ttt.py
+++ ttt.py
@@ -3,34 +3,35 @@ import sys
 from enum import Enum
 
 NO_PLAYER = 0
-board = [3 * [NO_PLAYER] for i in range(3)]
 
-def drawHLine(length):
+def draw_h_line(length):
   return (length * " ---") + '\n'
 
-def drawDataLine(dataLine):
-  return ''.join(map(lambda d: "| " + showPlayer(d) + " ", dataLine)) + "|\n"
+def draw_data_line(dataLine):
+  return ''.join(map(lambda d: "| " + show_player(d) + " ", dataLine)) + "|\n"
 
-def drawBoard(data):
+def draw_board(data):
   lineLen = len(data[0])
   return (
-    drawHLine(lineLen) +
-    ''.join(map(lambda dataLine: drawDataLine(dataLine) + drawHLine(lineLen), data)))
+    draw_h_line(lineLen) +
+    ''.join(map(lambda dataLine: draw_data_line(dataLine) +
+                                 draw_h_line(lineLen),
+                data)))
 
-def printBoard(board):
-  print(drawBoard(board))
+def print_board(board):
+  print(draw_board(board))
 
-def showPlayer(index):
+def show_player(index):
   return " XO"[index]
 
-def winningLine(line):
+def winning_line(line):
   if line[0] == line[1] and line[1] == line[2] and line[0] != NO_PLAYER:
     return line[0]
   return None
 
-def horizontalWin(board):
+def horizontal_win(board):
   for i in range(0, 3):
-    who = winningLine(board[i])
+    who = winning_line(board[i])
     if who != None:
       return who
   return None
@@ -38,23 +39,23 @@ def horizontalWin(board):
 def invert(board):
   return [[board[x][y] for x in range(3)] for y in range(3)]
 
-def verticalWin(board):
-  return horizontalWin(invert(board))
+def vertical_win(board):
+  return horizontal_win(invert(board))
 
-def diagonalWin(board):
-  who = winningLine([board[i][i] for i in range(3)])
+def diagonal_win(board):
+  who = winning_line([board[i][i] for i in range(3)])
   if who != None:
     return who
-  return winningLine([board[2 - i][i] for i in range(3)])
+  return winning_line([board[2 - i][i] for i in range(3)])
 
-def currentWinner(board):
-  for f in [horizontalWin, verticalWin, diagonalWin]:
+def current_winner(board):
+  for f in [horizontal_win, vertical_win, diagonal_win]:
     who = f(board)
     if who != None:
       return who
   return None
 
-def copyBoard(board):
+def copy_board(board):
   return [row.copy() for row in board]
 
 class Result(Enum):
@@ -62,78 +63,82 @@ class Result(Enum):
   WIN = 1
   LOSS = 2
 
-def bestResultAndMove(player, board):
-  assert currentWinner(board) == None
-  assert not boardFull(board)
+def best_result_and_move(player, board):
+  assert current_winner(board) == None
+  assert not board_full(board)
   bestForPlayer = (-1, -1, Result.LOSS) # We'll never use these coords.
   for row in range(3):
     for col in range(3):
       if board[row][col] == NO_PLAYER: # it's a legal move
-        newBoard = copyBoard(board)
+        newBoard = copy_board(board)
         newBoard[row][col] = player
-        winner = currentWinner(newBoard) 
+        winner = current_winner(newBoard) 
         if winner != None:
           assert winner == player
           return (row, col, Result.WIN)
-        if boardFull(newBoard):
+        if board_full(newBoard):
           bestForPlayer = (row, col, Result.TIE)
         else:
           (_, _, bestForOpponent) = \
-            bestResultAndMove(nextPlayer(player), newBoard)
+            best_result_and_move(next_player(player), newBoard)
           if bestForOpponent == Result.LOSS:
             return (row, col, Result.WIN)
           elif bestForOpponent == Result.TIE:
             bestForPlayer = (row, col, Result.TIE)
   return bestForPlayer
 
-def boardFull(board):
+def board_full(board):
   for row in board:
     for entry in row:
       if entry == NO_PLAYER:
         return False
   return True
 
-def nextPlayer(player):
+def next_player(player):
   return 3 - player
 
-def checkIfDone(board):
-  winner = currentWinner(board)
+def check_if_done(board):
+  winner = current_winner(board)
   if winner != None:
-    print("Player %s won." % showPlayer(winner))
+    print("Player %s won." % show_player(winner))
     return True
-  if (boardFull(board)):
+  if (board_full(board)):
     print("Game over, a tie.")
     return True
   return False
 
-def playAndCheck(board, player, r, c):
+def play_and_check(board, player, r, c):
   board[r][c] = player
-  player = nextPlayer(player)
-  printBoard(board)
-  done = checkIfDone(board)
+  player = next_player(player)
+  print_board(board)
+  done = check_if_done(board)
   return (player, board, done)
 
-player = 1
-print("Coordinates are 1-based, with [1,1] top left and [3, 1] top right.")
-done = False
-printBoard(board)
-while not done:
-  inString = input(
-    "Enter move coords for player %s as x,y > " % showPlayer(player))
-  try:
-    [x, y] = map(lambda s: int(s.strip()), inString.split(','))
-    cx = y-1
-    cy = x-1
-  except:
-    print("Bad input; try again.")
-    continue
-  if cx < 0 or cx > 2 or cy < 0 or cy > 2:
-    print("Illegal move--out of range.")
-    continue
-  if board[cx][cy]:
-    print("Illegal move--square already occupied.")
-    continue
-  (player, board, done) = playAndCheck(board, player, cx, cy)
-  if not done:
-    (r, c, _) = bestResultAndMove(player, board)
-    (player, board, done) = playAndCheck(board, player, r, c)
+if __name__ == '__main__':
+  print("Coordinates are 1-based, with [1,1] top left and [3, 1] top right.")
+
+  game_board = [3 * [NO_PLAYER] for i in range(3)]
+  player = 1
+  done = False
+  print_board(game_board)
+
+  while not done:
+    inString = input("Enter move coords for player %s as x,y > " %
+                     show_player(player))
+    try:
+      [x, y] = map(lambda s: int(s.strip()), inString.split(','))
+      cx = y-1
+      cy = x-1
+    except:
+      print("Bad input; try again.")
+      continue
+    if cx < 0 or cx > 2 or cy < 0 or cy > 2:
+      print("Illegal move--out of range.")
+      continue
+    if game_board[cx][cy]:
+      print("Illegal move--square already occupied.")
+      continue
+    (player, game_board, done) = play_and_check(game_board, player, cx, cy)
+    if not done:
+      (r, c, _) = best_result_and_move(player, game_board)
+      (player, game_board, done) = play_and_check(game_board, player, r, c)
