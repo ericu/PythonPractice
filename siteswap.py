e6ab1cce887348063fa8bca24bc1bb4a7ee7b814
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-09 19:09:03 -0700

    Clean up siteswap, add features.

--- siteswap.py
+++ siteswap.py
@@ -11,7 +11,7 @@ class SiteSwap:
   """Class for representing site-swap juggling patterns."""
 
   @staticmethod
-  def validatePatternGetBallCount(pattern):
+  def validate_pattern(pattern):
     if not all(map(lambda i: isinstance(i, int), pattern)):
       raise InputError(f'Pattern {pattern} contains non-integer value(s).')
     if not all(map(lambda i: i >= 0, pattern)):
@@ -29,37 +29,50 @@ class SiteSwap:
     assert(all(destinations))
     return balls
 
+  def from_string(str):
+    """Produces a SiteSwap from a comma-separated list of natural numbers."""
+    as_strings = [s.strip() for s in str.split(",")]
+    pattern = [int(i) for i in as_strings if len(i)]
+    return SiteSwap(pattern)
+
   def __init__(self, parsedPattern):
-    self.balls = SiteSwap.validatePatternGetBallCount(parsedPattern)
+    self.balls = SiteSwap.validate_pattern(parsedPattern)
     self.pattern = parsedPattern
+    self.hands = 2 # for now
+
+  def __repr__(self):
+    return f'SiteSwap({self.pattern!r})'
 
 class TestValidatePattern(unittest.TestCase):
   def test_simple_patterns(self):
-    self.assertEqual(3, SiteSwap.validatePatternGetBallCount([3]))
-    self.assertEqual(3, SiteSwap.validatePatternGetBallCount([2, 3, 4]))
-    self.assertEqual(3, SiteSwap.validatePatternGetBallCount([4, 4, 1]))
+    self.assertEqual(3, SiteSwap.validate_pattern([3]))
+    self.assertEqual(3, SiteSwap.validate_pattern([2, 3, 4]))
+    self.assertEqual(3, SiteSwap.validate_pattern([4, 4, 1]))
 
   def test_bad_input(self):
     with self.assertRaises(InputError):
-      SiteSwap.validatePatternGetBallCount([1.5, 2.5])
+      SiteSwap.validate_pattern([1.5, 2.5])
 
   def test_bad_ball_count(self):
     with self.assertRaises(InputError):
-      SiteSwap.validatePatternGetBallCount([3, 4])
+      SiteSwap.validate_pattern([3, 4])
 
   def test_collision(self):
     with self.assertRaises(InputError):
-      SiteSwap.validatePatternGetBallCount([4, 3, 2])
+      SiteSwap.validate_pattern([4, 3, 2])
 
-def getArgs():
+def get_args():
   name = sys.argv[0]
   parser = argparse.ArgumentParser()
   parser.add_argument("-u", "--unittest", help="run unit tests", action="store_true")
+  parser.add_argument("-t", "--test", help="run current test", action="store_true")
   args, argv = parser.parse_known_args()
   sys.argv[:] = [name] + argv
   return args
 
 if __name__ == '__main__':
-  args = getArgs()
+  args = get_args()
   if args.unittest:
     unittest.main()
+  elif args.test:
+    print(SiteSwap.from_string('4,4, 1'))
