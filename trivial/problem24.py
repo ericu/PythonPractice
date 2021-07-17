bb6e2b1f6762e202d054dcc4740bf9b058913c22
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-17 08:14:38 -0700

    More style cleanup.

--- trivial/problem24.py
+++ trivial/problem24.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 import sys
 
-val = input("input board size > ")
-# TODO: Catch conversion error.
-size = int(val)
-if size <= 0:
-    print("Board size must be an integer greater than zero")
-    sys.exit(-1)
-print("Got size: %d" % size)
 
-
-def horizontalLine(length):
+def horizontal_line(length):
     return (length * " ---") + "\n"
 
 
-def boardLine(dataLine):
-    return "".join(map(lambda d: "| " + d + " ", dataLine)) + "|\n"
+def board_line(data_line):
+    return "".join(map(lambda d: "| " + d + " ", data_line)) + "|\n"
 
 
-def fakeDataLine(size):
+def fake_data_line(size):
     return "." * size
 
 
-def fakeData(size):
-    return [fakeDataLine(size) for a in range(size)]
+def fake_data(size):
+    return [fake_data_line(size) for a in range(size)]
 
 
 def board(data):
-    lineLen = len(data[0])
-    return horizontalLine(lineLen) + "".join(
+    line_len = len(data[0])
+    return horizontal_line(line_len) + "".join(
         map(
-            lambda dataLine: boardLine(dataLine) + horizontalLine(lineLen),
+            lambda data_line: board_line(data_line) + horizontal_line(line_len),
             data,
         )
     )
 
 
-print(board(fakeData(size)))
+def main():
+    val = input("input board size > ")
+    # NOTE: conversion errors not handled.
+    size = int(val)
+    if size <= 0:
+        print("Board size must be an integer greater than zero")
+        sys.exit(-1)
+    print("Got size: %d" % size)
+
+    print(board(fake_data(size)))
+
+
+if __name__ == "__main__":
+    main()
