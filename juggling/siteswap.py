4feaf1c32bcb9b2bc47264939828afb7bd2fcec3
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-18 18:44:28 -0700

    Tweak hand placement for > 2 hands.

--- juggling/siteswap.py
+++ juggling/siteswap.py
@@ -134,7 +134,7 @@ def _simple_catch_pos(hand, num_hands):
     if num_hands == 2:
         return np.array([(hand - 0.5) * R * 2, R * 0.1])
     angle = hand / num_hands * 2 * math.pi
-    outer_r = R * 1.1
+    outer_r = R * 1.5
     return np.array([outer_r * math.cos(angle), outer_r * math.sin(angle)])
 
 
