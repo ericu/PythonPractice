c17b822fd8d8981fcfd4459befc08008bc674eeb
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-20 20:40:07 -0700

    Add missing number files, just in case.

--- trivial/problem23.py
+++ trivial/problem23.py
@@ -6,8 +6,8 @@ def get_numbers(filename):
         return [int(line.strip()) for line in input_file]
 
 
-primes = set(get_numbers("primenumbers.txt"))
-print("primes", primes)
-happies = set(get_numbers("happynumbers.txt"))
-print("happies", happies)
-print("intersection", primes.intersection(happies))
+numbers0 = set(get_numbers("numbers0.txt"))
+print("first file", numbers0)
+numbers1 = set(get_numbers("numbers1.txt"))
+print("second file", numbers1)
+print("intersection", numbers0.intersection(numbers1))
