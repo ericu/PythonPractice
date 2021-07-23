cfe4627f1044f68c72dca9ef6097e9fc42f9d8e2
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-23 09:09:56 -0700

    Depth testing working.

--- 3d/pyglet_sample.py
+++ 3d/pyglet_sample.py
@@ -16,6 +16,8 @@ class AppWindow(pyglet.window.Window):
     config = screen.get_best_config(template)
     super().__init__(config=config, resizable=True)
     self.cube = Cube((-0.5, 0.5), (-0.5, 0.5), (-0.5, 0.5))
+#    self.point = Point((0, 0, 0), (255, 128, 64))
+    self.cube2 = Cube((-0.1, 0.1), (-0.1, 0.1), (-0.1, 0.1), (255, 128, 64))
 
   def on_draw(self):
 
@@ -32,18 +34,7 @@ class AppWindow(pyglet.window.Window):
 #      glRotatef(45, 0, 1, 0)
 
       glEnable(GL_DEPTH_TEST)
-#      c0 = (-0.5, -0.5)
-#      c1 = (-0.5,  0.5)
-#      c2 = ( 0.5,  0.5)
-#      c3 = ( 0.5, -0.5)
-#      coords = c0 + c1 + c2 + c3
-#      pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
-#                                   [0, 1, 2, 0, 2, 3],
-#                                   ('v2f', coords),
-#                                   ('c3B', (0, 0, 255,
-#                                            0, 255, 0,
-#                                            255, 0, 0,
-#                                            0, 255, 255)))
+      self.cube2.draw()
       self.cube.draw()
 
   def on_resize(self, arg, arg2):
@@ -52,32 +43,45 @@ class AppWindow(pyglet.window.Window):
 
 
 class Cube():
-  def __init__(self, x_dims, y_dims, z_dims):
-    self.x_dims = x_dims
-    self.y_dims = y_dims
-    self.z_dims = z_dims
-
-  def draw(self):
-    coords = tuple(concat([(x, y, z) for x in self.x_dims
-                                     for y in self.y_dims
-                                     for z in self.z_dims]))
-    indices = [
+  def __init__(self, x_dims, y_dims, z_dims, color=None):
+    self.coords = tuple(concat([(x, y, z) for x in x_dims
+                                          for y in y_dims
+                                          for z in z_dims]))
+    self.indices = [
       0, 1, 3, 2, # Left wall
       4, 5, 7, 6, # Right wall
       2, 3, 7, 6, # Ceiling
       0, 1, 5, 4, # Floor
       0, 2, 6, 4 # Back wall
     ]
-#    side_color = (128, 128, 128)
-    floor_color = (64, 64, 64)
-    ceiling_color = (192, 192, 192)
-    vertex_count = len(coords) // 3
-    colors = tuple((2 * floor_color + 2 * ceiling_color) * 2)
-    pyglet.graphics.draw_indexed(vertex_count, pyglet.gl.GL_QUADS,
-                                 indices,
-                                 ('v3f', coords),
-                                 ('c3B', colors))
+    if color:
+      floor_color = color
+      ceiling_color = color
+    else:
+      floor_color = (64, 64, 64)
+      ceiling_color = (192, 192, 192)
+    self.colors = tuple((2 * floor_color + 2 * ceiling_color) * 2)
+    self.vertex_count = len(self.coords) // 3
 
+  def draw(self):
+    pyglet.graphics.draw_indexed(self.vertex_count, pyglet.gl.GL_QUADS,
+                                 self.indices,
+                                 ('v3f', self.coords),
+                                 ('c3B', self.colors))
+
+class Point():
+  def __init__(self, coords, color):
+    self.coords = coords
+    self.color = color
+    print('coords', self.coords)
+    print('color', self.color)
+    pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
+                         ('v3f', self.coords),
+                         ('c3B', self.color))
+
+  def draw(self):
+    pass
+    
 
 if __name__ == '__main__':
   window = AppWindow()
