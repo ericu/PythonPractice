4dd6084c78ad6edd134628601e2d2977d67b2cee
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-07-23 08:59:23 -0700

    Hazy cube displaying.

--- 3d/pyglet_sample.py
+++ 3d/pyglet_sample.py
@@ -23,27 +23,27 @@ class AppWindow(pyglet.window.Window):
 
       glMatrixMode(GL_PROJECTION)
       glLoadIdentity()
-      gluPerspective(90, 1, 0.1, 100)
+      gluPerspective(95, 1, 0.1, 100)
 
       glMatrixMode(GL_MODELVIEW)
       glLoadIdentity()
       glTranslatef(0, 0, -1)
       # Angle, axis
-      glRotatef(45, 0, 1, 0)
+#      glRotatef(45, 0, 1, 0)
 
       glEnable(GL_DEPTH_TEST)
-      c0 = (-0.5, -0.5)
-      c1 = (-0.5,  0.5)
-      c2 = ( 0.5,  0.5)
-      c3 = ( 0.5, -0.5)
-      coords = c0 + c1 + c2 + c3
-      pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
-                                   [0, 1, 2, 0, 2, 3],
-                                   ('v2f', coords),
-                                   ('c3B', (0, 0, 255,
-                                            0, 255, 0,
-                                            255, 0, 0,
-                                            0, 255, 255)))
+#      c0 = (-0.5, -0.5)
+#      c1 = (-0.5,  0.5)
+#      c2 = ( 0.5,  0.5)
+#      c3 = ( 0.5, -0.5)
+#      coords = c0 + c1 + c2 + c3
+#      pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
+#                                   [0, 1, 2, 0, 2, 3],
+#                                   ('v2f', coords),
+#                                   ('c3B', (0, 0, 255,
+#                                            0, 255, 0,
+#                                            255, 0, 0,
+#                                            0, 255, 255)))
       self.cube.draw()
 
   def on_resize(self, arg, arg2):
@@ -61,14 +61,18 @@ class Cube():
     coords = tuple(concat([(x, y, z) for x in self.x_dims
                                      for y in self.y_dims
                                      for z in self.z_dims]))
-    print(coords)
     indices = [
-      0, 1, 3, 2,
-      4, 5, 8, 6, # quads are double-sided, so we can be lazy
+      0, 1, 3, 2, # Left wall
+      4, 5, 7, 6, # Right wall
+      2, 3, 7, 6, # Ceiling
+      0, 1, 5, 4, # Floor
+      0, 2, 6, 4 # Back wall
     ]
+#    side_color = (128, 128, 128)
+    floor_color = (64, 64, 64)
+    ceiling_color = (192, 192, 192)
     vertex_count = len(coords) // 3
-    print('vertex_count', vertex_count)
-    colors = tuple([80, 80, 80] * vertex_count)
+    colors = tuple((2 * floor_color + 2 * ceiling_color) * 2)
     pyglet.graphics.draw_indexed(vertex_count, pyglet.gl.GL_QUADS,
                                  indices,
                                  ('v3f', coords),
@@ -77,7 +81,6 @@ class Cube():
 
 if __name__ == '__main__':
   window = AppWindow()
-  print(f'dimensions ({window.width},{window.height})')
   context = window.context
   config = context.config
 #  print(config)
