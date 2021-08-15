b1770b6787e454b49061e6cc38d70ba07e56c744
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2021-08-15 15:23:16 -0700

    Add type hints.

--- snake/snake.py
+++ snake/snake.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import List, Dict, Optional
 import curses
 import random
 import time
@@ -24,17 +25,18 @@ class WinException(Exception):
 
 
 class Player:
-    def __init__(self, coords, length):
+    def __init__(self, coords: List[int], length: int):
         self.coords = coords
+        self.drawings: List[List[int]]
         self.drawings = []
         self.add_length = length
         self.v = [0, 1]
 
-    def set_coords(self, coords):
+    def set_coords(self, coords: List[int]) -> None:
         self.drawings.append(coords)
         self.coords = coords
 
-    def trim(self):
+    def trim(self) -> Optional[List[int]]:
         if self.add_length > 0:
             self.add_length -= 1
             return None
@@ -44,7 +46,7 @@ class Player:
 
 
 class SnakeGame:
-    def __init__(self, window, height, width):
+    def __init__(self, window, height: int, width: int):
         self.done = False
         self.window = window
         self.window.nodelay(True)
@@ -54,6 +56,7 @@ class SnakeGame:
 
         self.status_bar = curses.newwin(1, width - 2, 1, 1)
 
+        self.poison_locations: Dict[str, bool]
         self.poison_locations = {}
         game_width = width - 2  # space for borders
         game_height = height - 4  # space for borders and status
@@ -65,7 +68,7 @@ class SnakeGame:
         self.place_poison()
         self.game_area.refresh()
 
-    def pick_clear_location(self):
+    def pick_clear_location(self) -> List[int]:
         (height, width) = self.game_area.getmaxyx()
 
         def pick_location():
@@ -75,35 +78,35 @@ class SnakeGame:
 
         coords = pick_location()
         while (str(coords) in self.poison_locations) or (
-            self.game_area.inch(coords[0], coords[1]) & 0xFF
+            self.game_area.inch(coords[0], coords[1]) & 0xFF  # type: ignore
         ) != ord(" "):
             coords = pick_location()
         return coords
 
-    def place_door(self):
+    def place_door(self) -> None:
         door_location = self.pick_clear_location()
         self.draw_char(door_location, DOOR_CHAR)
 
-    def place_food(self):
+    def place_food(self) -> None:
         food_location = self.pick_clear_location()
         self.draw_char(food_location, FOOD_CHAR)
 
-    def place_poison(self):
+    def place_poison(self) -> None:
         location = self.pick_clear_location()
         self.poison_locations[str(location)] = True
         self.draw_char(location, POISON_CHAR)
 
-    def draw_char(self, coords, char):
+    def draw_char(self, coords: List[int], char: str) -> None:
         (height, width) = self.game_area.getmaxyx()
         y, x = coords
-        # Curses can't addch to the bottom-right corner without ERR.
+        # Curses can't addch to the bottom right corner without ERR.
         # https://stackoverflow.com/questions/36387625/curses-fails-when-calling-addch-on-the-bottom-right-corner
         if y == height - 1 and x == width - 1:
             self.game_area.insch(y, x, char)
         else:
             self.game_area.addch(y, x, char)
 
-    def draw_player(self, coords):
+    def draw_player(self, coords: List[int]) -> None:
         self.draw_char(self.player.coords, "s")
         self.player.set_coords(coords)
         self.draw_char(self.player.coords, "S")
@@ -114,7 +117,7 @@ class SnakeGame:
             y, x = to_erase
             self.game_area.addch(y, x, " ")
 
-    def move_player(self):
+    def move_player(self) -> None:
         (height, width) = self.game_area.getmaxyx()
         p_y, p_x = self.player.coords
         v_y, v_x = self.player.v
@@ -127,7 +130,7 @@ class SnakeGame:
             raise DeathException("You ate the poison.")
 
         need_more_food = False
-        hit_char = self.game_area.inch(p_y, p_x) & 0xFF
+        hit_char = self.game_area.inch(p_y, p_x) & 0xFF  # type: ignore
         if hit_char == ord(" "):
             pass
         elif hit_char == ord(FOOD_CHAR):
@@ -143,7 +146,7 @@ class SnakeGame:
             self.place_food()
             self.place_poison()
 
-    def die(self, cause_of_death):
+    def die(self, cause_of_death: str) -> None:
         for coords in self.player.drawings:
             self.draw_char(coords, "x")
         self.draw_char(self.player.coords, "X")
@@ -151,13 +154,13 @@ class SnakeGame:
         self.set_status(cause_of_death + "  You have died.")
         self.done = True
 
-    def win(self):
+    def win(self) -> None:
         self.set_status(
             f"You have gotten away with {len(self.player.drawings)} points."
         )
         self.done = True
 
-    def play(self):
+    def play(self) -> None:
         next_draw = time.time() + MOVE_PERIOD_SECONDS
         while True:
             time.sleep(SLEEP_TIME)
@@ -189,7 +192,7 @@ class SnakeGame:
             except WinException:
                 self.win()
 
-    def draw_borders(self):
+    def draw_borders(self) -> None:
         self.window.clear()
         self.window.border()
         self.window.hline(2, 1, curses.ACS_HLINE, self.width - 2)
@@ -197,7 +200,7 @@ class SnakeGame:
         self.window.addch(2, self.width - 1, curses.ACS_RTEE)
         self.window.refresh()
 
-    def set_status(self, status):
+    def set_status(self, status: str) -> None:
         self.status_bar.clear()
         # Leave 1 space at each end, both for style and due to the curses
         # last-char-in-window wrapping exception.
